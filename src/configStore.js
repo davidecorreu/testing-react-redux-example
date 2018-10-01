f4c273f72f07c29a9133c2b6f0259ce548260e4e
David Rebolleda Mir <david@MacBook-Pro-de-David.local>
2018-10-01 18:04:45 +0200

    create async middleware

--- src/configStore.js
+++ src/configStore.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
-import reduxPromise from 'redux-promise';
+// import reduxPromise from 'redux-promise';
+import async from 'middlewares/async';
 
 
 export default (initialState = {}) => {
   return createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
-    // applyMiddleware()
+    // applyMiddleware(reduxPromise)
+    applyMiddleware(async)
   );
 };
--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,27 @@
-export default function({ dispatch }) {
-  return function(next) {
-    return function(action) {
-      
-    }
+// export default function({ dispatch }) {
+//   return function(next) {
+//     return function(action) {
+//
+//     }
+//   }
+// }
+
+export default ({dispatch}) => next => action => {
+  // Check to see if the actions
+  // has a promise on its 'payload' property
+  // If it does, then wait for it to resolve
+  // If it dosen't, then send the action on then
+  // next middleware
+
+  if(!action.payload || !action.payload.then) {
+    return next(action);
   }
+
+  // We want to wait for the promise to resolve
+  // (get its data!!) and then create a new actions
+  // with that data and dispatch it
+  action.payload.then(response => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  })
 }
