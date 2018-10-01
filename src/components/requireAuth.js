d096328de93b844a879d97ef83da76680ca70995
David Rebolleda Mir <david@MacBook-Pro-de-David.local>
2018-10-01 17:28:35 +0200

    middleware boilerplate

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -24,7 +24,6 @@ export default (ChildComponent) => {
     }
   }
 
-
   const mapStateToProps = state => ({
     auth: state.auth
   });
--- src/configStore.js
+++ src/configStore.js
@@ -8,5 +8,6 @@ export default (initialState = {}) => {
     reducers,
     initialState,
     applyMiddleware(reduxPromise)
+    // applyMiddleware()
   );
 };
