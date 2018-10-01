2679e2eb4c0942700d8ae411154be4d0f03f5a9d
David Rebolleda Mir <david@MacBook-Pro-de-David.local>
2018-10-01 15:03:18 +0200

    add requireAuth HOC

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-export default () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
+class App extends Component {
+  renderButton() {
+    const { changeAuth } = this.props;
+    if (this.props.auth) {
+      return (
+        <button onClick={() => changeAuth(false)}>
+          Sing Out
+        </button>);
+    } else {
+      return (
+        <button onClick={() => changeAuth(true)}>
+          Sign In
+        </button>);
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='post'>Post A Comment</Link></li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path='/post' component={CommentBox} />
+        <Route path='/' exact component={CommentList} />
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+
+export default connect(mapStateToProps, actions)(App)
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
+import requireAuth from 'components/requireAuth';
 
 class CommentBox extends Component {
   constructor(props) {
@@ -21,6 +22,7 @@ class CommentBox extends Component {
   }
 
   render() {
+    console.log('CommentBox this.props:', this.props);
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -39,4 +41,4 @@ class CommentBox extends Component {
   }
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
